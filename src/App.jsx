827d47afac715fdbb42760b655df84e5ed3c3bae
Ofarouq310 <ofarouq310@gmail.com>
2025-07-14 16:10:45 +0300

    Import website components and sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-
+import Nav from "./components/Nav";
+import {Hero, Popular, SuperQuality,
+  Services, SpecialOffer, CustomerReview,
+  Subscribe, Footer} from "./sections/index";
 
 export default function App (){
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-center">
-        Nike Clone
-      </h1>
-    </>
+    <main className="w-full h-full relative">
+      <Nav />
+      <section className="">
+        <Hero />  
+      </section>
+      <section className="section-padding">
+        <Popular />
+      </section>
+      <section className="section-padding">
+        <SuperQuality />  
+      </section>
+      <section className="section-padding">
+        <Services />
+      </section>
+      <section className="section-padding">
+        <SpecialOffer />
+      </section>
+      <section className="bg-pale-blue section-padding">
+        <CustomerReview />
+      </section>
+      <section className="padding-x sm:py-32 p-16 w-full">
+        <Subscribe />
+      </section>
+      <Footer />
+    </main>
   )
 }
