f317234a16bb829357dce11f64b2969fd4111ed8
Ofarouq310 <ofarouq310@gmail.com>
2025-07-14 16:14:09 +0300

    Add services and customer reviews constants objects

---
+++ src/constants/index.js
@@ -0,0 +1,30 @@
+import { shieldTick, truckFast, support } from "../assets/icons/index.js";
+import { customer1, customer2 } from "../assets/images/index.js";
+
+export const services = [{
+    icon: truckFast,
+    title: "Fast Delivery",
+    description: "Get your orders delivered within 24 hours with our express shipping."
+}, {
+    icon: shieldTick,
+    title: "Secure Payments",
+    description: "We ensure secure transactions with our advanced encryption technology."
+},  {
+    icon: support,
+    title: "24/7 Customer Support",
+    description: "Our customer support team is available 24/7 to assist you."
+}];
+
+
+export const customerReviews = [{
+    photo: customer1,
+    name: "Morich Brown",
+    rating: 4.5,
+    review: "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!"
+}, {
+    photo: customer2,
+    name: "Lota Mongeskar",
+    rating: 4.5,
+    review: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
+}];
+
