6dffe7e46b177fbc5cbd56ba9fe86ec054cd3cc5
Ofarouq310 <ofarouq310@gmail.com>
2025-07-14 16:17:08 +0300

    Add combined export file for website sections

---
+++ src/sections/index.js
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+import Popular from "./Popular";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReview from "./CustomerReview";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export {Hero, Popular, SuperQuality,
+Services, SpecialOffer, CustomerReview,
+Subscribe, Footer};
